5a9be7d8579430ec707d0c89cace9bd11cf39a6b
Emanoil Karov <e_karov@yahoo.com>
2022-11-25 18:13:32 +0100

    Implement the listing of all cars and add css file

--- carsAPI/db_check.php
+++ carsAPI/db_check.php
@@ -5,10 +5,13 @@ $sql = "SELECT * FROM car";
 
 $result = mysqli_query($connection, $sql);
 
-$row = mysqli_fetch_assoc($result);
-
-$name = @$row['name'] ?: null;
-$price = @$row['price'] ?: null;
-$picture = @$row['picture'] ?: null;
-
+if (mysqli_num_rows($result) > 0) {
+    $resultArr = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $name = @$row['name'] ?: null;
+        $price = @$row['price'] ?: null;
+        $picture = @$row['picture'] ?: null;
+        array_push($resultArr, $row);
+    }
+}
 mysqli_close($connection);
--- carsAPI/webservice.php
+++ carsAPI/webservice.php
@@ -3,29 +3,22 @@
 header("Content-Type", "application/json");
 
 if (isset($_GET)) {
-    // $id = $_GET['id'];
 
     require_once("db_check.php");
-
     if (empty($name) && empty($price)) {
         response(200, "Car not found");
     } else {
-        response(200, "Car found", $name, $price, $picture);
+        response(200, "Car found", $resultArr);
     }
 } else {
     response(400, "Invalid request");
 }
 
-function response($status, $status_message, ...$data)
+function response($status, $status_message, $data=null)
 {
-    $keys = ["name", "price", "picture"];
-
     $response['status'] = $status;
     $response['status_message'] = $status_message;
-    for ($i = 0; $i < count($data); $i++) {
-        $response[$keys[$i]] = $data[$i];
-    }
-
+    $response['data'] = $data;
     $json_response = json_encode($response);
 
     echo $json_response;
