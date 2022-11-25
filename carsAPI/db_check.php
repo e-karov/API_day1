0560e8f0cdc65509fb0265cc2022ae7664a305df
Emanoil Karov <e_karov@yahoo.com>
2022-11-25 11:30:41 +0100

    Add multiple cars to collection

--- carsAPI/db_check.php
+++ carsAPI/db_check.php
@@ -5,10 +5,11 @@ $sql = "SELECT * FROM car";
 
 $result = mysqli_query($connection, $sql);
 
-$row = mysqli_fetch_assoc($result);
 
-$name = @$row['name'] ?: null;
-$price = @$row['price'] ?: null;
-$picture = @$row['picture'] ?: null;
+$data = mysqli_fetch_all($result);
+
+// $name = @$row['name'] ?: null;
+// $price = @$row['price'] ?: null;
+// $picture = @$row['picture'] ?: null;
 
 mysqli_close($connection);
--- carsAPI/webservice.php
+++ carsAPI/webservice.php
@@ -1,13 +1,23 @@
 <?php
+/****************************************
+*######## RESTful WEB SERVICE ##########*
+*                                       *
+* Client process the request VIA URL    *
+* http://address.com/webservice.php?id=1*
+* and gets the JSON result.             *
+****************************************/
 
+// Set Content-Type header to application/json
 header("Content-Type", "application/json");
 
 if (isset($_GET)) {
-    // $id = $_GET['id'];
-
+# if (!empty($_GET['id'])){
+#   $id = $_GET['id'];...
+#   ...
+# }
     require_once("db_check.php");
 
-    if (empty($name) && empty($price)) {
+    if (empty($data) ) {
         response(200, "Car not found");
     } else {
         response(200, "Car found", $name, $price, $picture);
@@ -22,10 +32,11 @@ function response($status, $status_message, ...$data)
 
     $response['status'] = $status;
     $response['status_message'] = $status_message;
-    for ($i = 0; $i < count($data); $i++) {
-        $response[$keys[$i]] = $data[$i];
+    foreach ($data as $car) {
+        for ($i = 0; $i < count($data); $i++) {
+            $response[$keys[$i]] = $data[$i];
+        }
     }
-
     $json_response = json_encode($response);
 
     echo $json_response;
